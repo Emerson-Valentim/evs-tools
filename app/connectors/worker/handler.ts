024741e26cae9168b8fddc8a4f41600ec91e672a
Emerson Valentim <emevalentim0@gmail.com>
2022-01-02 12:47:55 -0300

    feat: add worker handler class

---
+++ app/connectors/worker/handler.ts
@@ -0,0 +1,19 @@
+import { JobsOptions, Queue } from "bullmq";
+import { Redis } from "ioredis";
+
+export class WorkerHandler<JobPayload> {
+  private connection: Redis;
+  private queueName: string;
+  private queue: Queue;
+
+  constructor(queueName: string, connection: Redis) {
+    this.queueName = queueName;
+    this.connection = connection;
+
+    this.queue = new Queue(this.queueName, { connection: this.connection });
+  }
+
+  public async add(data: JobPayload, options?: JobsOptions) {
+    await this.queue.add(this.queueName, data, options);
+  }
+}
